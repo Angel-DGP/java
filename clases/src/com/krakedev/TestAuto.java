4febcbdf97d470a88cb00caf5649cbbd0320cc7c
Angel-DGP <angel.patinogd@gmail.com>
2024-09-29 19:55:11 -0500

    Paso 1 y 2 realizados

---
+++ clases/src/com/krakedev/TestAuto.java
@@ -0,0 +1,30 @@
+package com.krakedev;
+
+public class TestAuto {
+
+	public static void main(String[] args) {
+		Auto auto1;
+		auto1 = new Auto();
+		Auto auto2;
+		auto2 = new Auto();
+		
+		auto1.Marca = "Ford";
+		auto1.anio = 2022;
+		auto1.precio = 35657.65;
+		
+		System.out.println("Marca auto1 :" + auto1.Marca);
+		System.out.println("Anio auto1 :" + auto1.anio);
+		System.out.println("Precio auto1 :" + auto1.precio);
+	
+		System.out.println("-----------------");
+		
+		auto2.Marca = "Chevrolet";
+		auto2.anio = 2022;
+		auto2.precio = 32997.95;
+		
+		System.out.println("Marca auto2 :" + auto2.Marca);
+		System.out.println("Anio auto2 :" + auto2.anio);
+		System.out.println("Precio auto2 :" + auto2.precio);
+	}
+
+}
